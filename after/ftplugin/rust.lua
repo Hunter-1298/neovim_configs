6f918e939632939a63935e11d9215b2dc05147e6
hhayden <hhayden@ltsnet.net>
2024-11-06 15:27:00 -0500

    added debugger and set custom after keymaps for rust

--- after/ftplugin/rust.lua
+++ after/ftplugin/rust.lua
@@ -2,12 +2,16 @@ local bufnr = vim.api.nvim_get_current_buf()
 vim.keymap.set('n', '<leader>a', function()
   vim.cmd.RustLsp 'codeAction' -- supports rust-analyzer's grouping
   -- or vim.lsp.buf.codeAction() if you don't want grouping.
-end, { silent = true, buffer = bufnr })
+end, { silent = true, buffer = bufnr, desc = 'Ruct Code Action - suggests changes' })
+
 vim.keymap.set(
   'n',
   'K', -- Override Neovim's built-in hover keymap with rustaceanvim's hover actions
   function()
     vim.cmd.RustLsp { 'hover', 'action' }
   end,
-  { silent = true, buffer = bufnr }
+  { silent = true, buffer = bufnr, desc = 'Overrides neovim built in over with rust hover' }
 )
+
+--rustacean nvim keybinding
+vim.keymap.set('n', '<leader>dt', '<cmd>lua vim.cmd("RustLsp testables")<CR>', { desc = 'Debugger testables' })
