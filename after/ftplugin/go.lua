c3e569f46c920ee502bb044da4d2487a14633a27
hhayden <hhayden@ltsnet.net>
2024-11-14 23:36:09 -0500

    Adding keybind for go error handling in /after dir

---
+++ after/ftplugin/go.lua
@@ -0,0 +1,8 @@
+local bufnr = vim.api.nvim_get_current_buf()
+--go nvim keybinding
+vim.api.nvim_set_keymap(
+  'n', -- Insert mode
+  '<leader>g', -- Keybinding (Ctrl+e, or choose your own)
+  'oif err !=  nil {<CR><TAB>fmt.Println("err: %v", err)<CR>}', -- Opens a new line and inserts the snippet with newlines
+  { noremap = true, silent = true, desc = 'Go error' }
+)
