8ef677481641a6a12fd6d324cf884c3ee60333e4
hhayden <hhayden@ltsnet.net>
2024-11-19 16:08:28 +0000

    added lazygit

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -3,6 +3,6 @@ local bufnr = vim.api.nvim_get_current_buf()
 vim.api.nvim_set_keymap(
   'n', -- Insert mode
   '<leader>g', -- Keybinding (Ctrl+e, or choose your own)
-  'oif err !=  nil {<CR><TAB>fmt.Println("err: %v", err)<CR>}', -- Opens a new line and inserts the snippet with newlines
+  'oif err !=  nil {<CR><TAB>fmt.Println("err: %v", err)<ESC>}', -- Opens a new line and inserts the snippet with newlines
   { noremap = true, silent = true, desc = 'Go error' }
 )
---
+++ lua/custom/plugins/lazygit.lua
@@ -0,0 +1,24 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  -- amongst your other plugins
+  {
+    'kdheepak/lazygit.nvim',
+    lazy = true,
+    cmd = {
+      'LazyGit',
+      'LazyGitConfig',
+      'LazyGitCurrentFile',
+      'LazyGitFilter',
+      'LazyGitFilterCurrentFile',
+    },
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    keys = {
+      { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+    },
+  },
+}
